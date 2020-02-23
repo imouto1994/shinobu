0a36fef67dc858caa879b336dd4684cd5ad297dc
Nhan Bui <imouto1994@gmail.com>
2020-02-24 01:08:38 +0800

    Ignore book from a store if it is too larger than the min price

--- src/worker/partitionBooksMaxPriority.ts
+++ src/worker/partitionBooksMaxPriority.ts
@@ -4,9 +4,26 @@ import { Map } from "../utils/type";
 const MAX_PRICE_PER_BUCKET = 25000;
 
 export function partitionBooks(books: Book[]): Map<Book[][]> {
-  const storeBooks: Map<{ total: number; books: Book[] }> = {};
+  // Populate `bookSourcesMap` & `bookMinPriceMap`
+  const bookSourcesMap: Map<Map<BookSource>> = {};
+  const minPriceMap: Map<number> = {};
+
+  for (const book of books) {
+    bookSourcesMap[book.id] = {};
+    minPriceMap[book.id] = book.sources.reduce(
+      (minPrice: number, source: BookSource): number => {
+        return Math.min(minPrice, source.price);
+      },
+      Infinity,
+    );
+
+    for (const source of book.sources) {
+      bookSourcesMap[book.id][source.storeName] = source;
+    }
+  }
 
   // Populate `storeBooks`
+  const storeBooks: Map<{ total: number; books: Book[] }> = {};
   for (const book of books) {
     for (const source of book.sources) {
       if (storeBooks[source.storeName] == null) {
@@ -18,15 +35,6 @@ export function partitionBooks(books: Book[]): Map<Book[][]> {
 
       storeBooks[source.storeName].total += source.price;
       storeBooks[source.storeName].books.push(book);
-    }
-  }
-
-  // Populate `bookSourcesMap`
-  const bookSourcesMap: Map<Map<BookSource>> = {};
-  for (const book of books) {
-    bookSourcesMap[book.id] = {};
-    for (const source of book.sources) {
-      bookSourcesMap[book.id][source.storeName] = source;
     }
   }
 
@@ -67,6 +75,11 @@ export function partitionBooks(books: Book[]): Map<Book[][]> {
         continue;
       }
       const bookPrice = bookSourcesMap[book.id][storeName].price;
+      const minPrice = minPriceMap[book.id];
+      // Ignore if book's price is larger than the minimum price for 1,000 yen
+      if (bookPrice - minPrice > 1000) {
+        continue;
+      }
       if (price + bookPrice > MAX_PRICE_PER_BUCKET) {
         continue;
       }
